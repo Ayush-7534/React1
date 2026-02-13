034310efe3e1e9cd9a432a9cc2f8274411c06988
Ayush Kumar Dhingra <ayushkumardhingra@gmail.com>
2026-02-13 16:36:17 +0530

    13-02-2026

---
+++ EventApp/src/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+export default function Counter(){
+    const [count,setCount] = useState(0);
+
+    function handleIncreament(){
+        setCount(count => count + 1);
+        console.log(count);
+    }
+
+    function handleDecrement(){
+        if(count === 0){
+            alert("Cant decrease now");
+            return;
+        }
+        setCount(count => count - 1);
+        console.log(count);
+    }
+    return (
+        <>
+        <h1> Counter App</h1>
+        <h3>Count={count}</h3>
+        <button onClick={handleIncreament}> + </button>
+        <button onClick={handleDecrement}> - </button>
+        </>
+    );
+}
--- EventApp/src/EventToDo.jsx
+++ EventApp/src/EventToDo.jsx
@@ -1,32 +1,38 @@
-export default function EventToDo(){
-    // function handleClick(name){
-    //     alert(`You prssed button ${name}`);
-    // }
-    function handleSubmit(e){
-        e.preventDefault();          // to stop reload of page
-        const input = e.target.value;
-        const inputValue = input.trim();
-        if(inputValue===""){
-            alert("Task can not be empty");
-            return;
-        }
+import { useState } from "react"
 
-        const li = document.createElement("li");
-        li.innerHTML = <span>${inputValue}</span>
+export default function EventToDo(){
+    // function handleSubmit(e){
+    //     e.preventDefault();          // to stop reload of page
+    //     const input = e.target.task;
+    //     const inputValue = input.value.trim();
+    //     if(inputValue === ""){
+    //         alert("Task can not be empty");
+    //         return;
+    //     }
+
+    //     const li = document.createElement("li");
+    //     li.innerHTML = `<span>${inputValue}</span>
+    //     <button class="deleteBtn">Delete</button>`;
+    //     document.getElementById("taskList").appendChild(li);
         
-    }
+    // }
+
+    // function handleClickList(e){
+    //     if(e.target.className==="deleteBtn"){
+    //         e.target.parentElement.remove();
+    //     }
+    // }
+    const[task,setTask]  = useState("");
+    const[tasks,setTasks]= useState("");
 
     return(
         <>
-        <h1>Event Handling</h1>
-        <button onClick={()=>handleClick("Ayush")}>Click Me</button>
 
-        <h1>Event to Do</h1>
-        <button onClick={()=>handleClick("Ayush")}>Click Me</button>
+        <h1>Event To Do</h1>
 
         <form onSubmit={handleSubmit}>
-            <input type="text" name="task" placeholder="Add your task here"/>
-            <input type="submit">Add Task</input>
+            <input type="text" name="task" placeholder="Enter Task" value={}/>
+            <button type="submit">Add Task</button>
     
         </form>
 
