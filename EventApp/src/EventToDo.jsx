86ecb9c64822f8c0ce259c102bf825e2ad9398e1
Ayush Kumar Dhingra <ayushkumardhingra@gmail.com>
2026-02-14 22:09:37 +0530

    14-02-2026

--- EventApp/src/EventToDo.jsx
+++ EventApp/src/EventToDo.jsx
@@ -1,27 +1,27 @@
 import { useState } from "react"
 
 export default function EventToDo(){
-    // function handleSubmit(e){
-    //     e.preventDefault();          // to stop reload of page
-    //     const input = e.target.task;
-    //     const inputValue = input.value.trim();
-    //     if(inputValue === ""){
-    //         alert("Task can not be empty");
-    //         return;
-    //     }
+    function handleSubmit(e){
+        e.preventDefault();          // to stop reload of page
+        const input = e.target.task;
+        const inputValue = input.value.trim();
+        if(inputValue === ""){
+            alert("Task can not be empty");
+            return;
+        }
 
-    //     const li = document.createElement("li");
-    //     li.innerHTML = `<span>${inputValue}</span>
-    //     <button class="deleteBtn">Delete</button>`;
-    //     document.getElementById("taskList").appendChild(li);
+        const li = document.createElement("li");
+        li.innerHTML = `<span>${inputValue}</span>
+        <button class="deleteBtn">Delete</button>`;
+        document.getElementById("taskList").appendChild(li);
         
-    // }
+    }
 
-    // function handleClickList(e){
-    //     if(e.target.className==="deleteBtn"){
-    //         e.target.parentElement.remove();
-    //     }
-    // }
+    function handleClickList(e){
+        if(e.target.className==="deleteBtn"){
+            e.target.parentElement.remove();
+        }
+    }
     const[task,setTask]  = useState("");
     const[tasks,setTasks]= useState("");
 
